b04472c2efa2483213fb27518561124449d53f2d
Conary36 <cbeckford144@gmail.com>
2020-11-08 22:53:49 -0500

    ..intersection, slow time...

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,6 +3,13 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    set_neg = set()
+    for i in a:
+        set_neg.add(i)
+    for i in a:
+        if(i > 0 and -1*i in set_neg):
+            result.append(i)
 
     return result
 
